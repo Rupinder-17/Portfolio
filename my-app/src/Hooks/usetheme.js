da6468404cf7cd0918e11914219a1bbe41d0f9c7
Rupinder-17 <rupinderrandhey90@gmail.com>
2024-12-22 20:39:11 +0530

    apply theme

--- my-app/src/Hooks/usetheme.js
+++ my-app/src/Hooks/usetheme.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import  { useContext } from 'react'
 import { MyTheme } from './ContextHook'
 
 export const Usetheme = () => {
--- my-app/src/component/Header.jsx
+++ my-app/src/component/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import { Usetheme } from "@/Hooks/usetheme";
+import React from "react";
+import { FiMoon } from "react-icons/fi";
 
 export const Header = () => {
+  const { theme, handleTheme, color } = Usetheme();
   return (
     <div>
       <header className="bg-white shadow-md">
         <nav className="container mx-auto px-6 py-3 flex justify-between items-center">
-          <div></div>
+          {/* <div> */}
+          <FiMoon
+            onClick={handleTheme}
+            style={{ background: color.background, color: color.color }}
+          />
+          {/* </div> */}
           <div className="hidden md:flex space-x-4">
             <a href="#about" className="text-red-600 hover:text-gray-800">
               About
@@ -21,6 +29,4 @@ export const Header = () => {
       </header>
     </div>
   );
-}
-
-
+};

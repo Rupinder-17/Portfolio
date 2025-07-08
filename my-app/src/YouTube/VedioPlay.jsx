ae5ee344f909ecc315428ba73e9e4945490587c6
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-07-08 17:28:20 +0530

    feat: refactor VedioPlay component layout for improved responsiveness and styling

--- my-app/src/YouTube/VedioPlay.jsx
+++ my-app/src/YouTube/VedioPlay.jsx
@@ -17,45 +17,50 @@ export const VedioPlay = () => {
   }
 
   return (
-    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {vedioId && (
-        <iframe
-          width="100%"
-          height="200"
-          src={`https://www.youtube.com/embed/${vedioId}`}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          className="rounded-t-2xl"
-        ></iframe>
-      )}
-      {vedioData?.map((item, index) => {
-        
-          const vedioData = item.items
-         return <div
-            key={index}
-            className="bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300"
-          >
-            <img
-              src={vedioData.snippet?.thumbnails?.high?.url}
-              alt={item.snippet?.title}
-              className="w-full h-48 object-cover"
-              onClick={() => handlePlay(vedioData.id)}
-            />
-            <div className="p-4">
-              <h2 className="text-lg font-semibold mb-2 text-gray-800">
-                {vedioData.snippet?.channelTitle}
-              </h2>
-              <p className="text-sm text-gray-600 mb-1">
-                {/* {item.items.snippet?.description} */}
-              </p>
-              <span className="text-xs bg-red-100 text-red-700 px-2 py-1 rounded">
-                {item.kind}
-              </span>
+    <div className="p-4 flex gap-8 bg-slate-500 ">
+      <div className="bg-red-300 w-[50%]">
+        {vedioId && (
+          <iframe
+            width="100%"
+            height="200"
+            src={`https://www.youtube.com/embed/${vedioId}`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="rounded-t-2xl "
+          ></iframe>
+        )}
+      </div>
+
+      <div className=" w-[40%]">
+        {vedioData?.map((item, index) => {
+          const vedioData = item.items;
+          return (
+            <div
+              key={index}
+              className=" rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300"
+            >
+              <img
+                src={vedioData.snippet?.thumbnails?.high?.url}
+                alt={item.snippet?.title}
+                className=" object-contain"
+                onClick={() => handlePlay(vedioData.id)}
+              />
+              <div className="p-4">
+                <h2 className="text-lg font-semibold mb-2 text-gray-800">
+                  {vedioData.snippet?.channelTitle}
+                </h2>
+                <p className="text-sm text-gray-600 mb-1">
+                  {/* {item.items.snippet?.description} */}
+                </p>
+                <span className="text-xs bg-red-100 text-red-700 px-2 py-1 rounded">
+                  {item.kind}
+                </span>
+              </div>
             </div>
-          </div>
-        
-})}
+          );
+        })}
+      </div>
     </div>
   );
 };

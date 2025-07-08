f8f1292ae7be2f2f57f8d6df6b3b49f517dd29c3
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-07-08 16:49:57 +0530

    feat: enhance VedioPlay component to fetch and display YouTube video data

--- my-app/src/YouTube/Hooks/useApi.js
+++ my-app/src/YouTube/Hooks/useApi.js
@@ -6,12 +6,14 @@ export const useApi = () => {
   const [vedioData, setVedioData] = useState();
   const playVedio = async () => {
         try {
-      const res = await fetch(
-        "https://api.freeapi.app/api/v1/public/youtube/videos"
-      );
+      const res = await fetch("https://api.freeapi.app/api/v1/public/youtube/videos",{
+        method: 'GET',
+        headers: {accept: 'application/json'}
+
+      } );
       const data = await res.json();
       console.log(data);
-      setVedioData(data);
+      setVedioData(data.data.data);
     }
     catch (e) {
         console.log(e);
--- my-app/src/YouTube/VedioPlay.jsx
+++ my-app/src/YouTube/VedioPlay.jsx
@@ -1,7 +1,39 @@
-// import React from 'react'
+import { useEffect } from "react";
+import { useApi } from "./Hooks/useApi";
 
 export const VedioPlay = () => {
+  const { vedioData, playVedio } = useApi([]);
+  console.log("ved", vedioData);
+
+  useEffect(() => {
+    playVedio();
+  }, []);
+
   return (
-    <div>VedioPlay</div>
-  )
-}
+    <div className="p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      {vedioData?.map((item, index) => (
+        <div
+          key={index}
+          className="bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-xl transition-all duration-300"
+        >
+          <img
+            src={item.items.snippet?.thumbnails?.high?.url}
+            alt={item.snippet?.title}
+            className="w-full h-48 object-cover"
+          />
+          <div className="p-4">
+            <h2 className="text-lg font-semibold mb-2 text-gray-800">
+              {item.items.snippet?.channelTitle}
+            </h2>
+            <p className="text-sm text-gray-600 mb-1">
+              {/* {item.items.snippet?.description} */}
+            </p>
+            <span className="text-xs bg-red-100 text-red-700 px-2 py-1 rounded">
+              {item.kind}
+            </span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
